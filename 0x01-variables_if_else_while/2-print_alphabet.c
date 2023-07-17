a817a7af16a51ec7cba617f07e4f11acdd2b12a4
petersameh2023 <petersameh777@gmail.com>
2023-07-17 10:52:44 -0700

    finishing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Positive or negative
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+
+{
+	putchar(ch);
+	ch++;
+}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Positive or negative
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+{
+	putchar(ch);
+		ch++;
+}
+	while (CH <= 'Z')
+{
+	putchar(CH);
+		CH++;
+}
+	putchar('\n');
+	return (0);
+}
